41862c831333ac73a26d920cd5a127aae4f928cc
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2019-01-04 14:18:35 +0200

    Add a util method to get the first PropertyDirection from an IBlockState

--- src/main/java/fi/dy/masa/malilib/util/BlockUtils.java
+++ src/main/java/fi/dy/masa/malilib/util/BlockUtils.java
@@ -9,6 +9,25 @@ import net.minecraft.util.EnumFacing;
 public class BlockUtils
 {
     /**
+     * Returns the first PropertyDirection property from the provided state, if any.
+     * @param state
+     * @return the first PropertyDirection, or null if there are no such properties
+     */
+    @Nullable
+    public static PropertyDirection getFirstDirectionProperty(IBlockState state)
+    {
+        for (IProperty<?> prop : state.getProperties().keySet())
+        {
+            if (prop instanceof PropertyDirection)
+            {
+                return (PropertyDirection) prop;
+            }
+        }
+
+        return null;
+    }
+
+    /**
      * Returns the EnumFacing value of the first found PropertyDirection
      * type blockstate property in the given state, if any.
      * If there are no PropertyDirection properties, then null is returned.
@@ -18,14 +37,7 @@ public class BlockUtils
     @Nullable
     public static EnumFacing getFirstPropertyFacingValue(IBlockState state)
     {
-        for (IProperty<?> prop : state.getProperties().keySet())
-        {
-            if (prop instanceof PropertyDirection)
-            {
-                return state.getValue((PropertyDirection) prop);
-            }
-        }
-
-        return null;
+        PropertyDirection prop = getFirstDirectionProperty(state);
+        return prop != null ? state.getValue(prop) : null;
     }
 }
