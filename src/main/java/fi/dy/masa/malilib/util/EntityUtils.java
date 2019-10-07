d6c6dcc5b813f0e5b37f3319fa27ad3368110c07
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2019-10-07 22:41:42 +0300

    Add an EntityUtils util class

---
+++ src/main/java/fi/dy/masa/malilib/util/EntityUtils.java
@@ -0,0 +1,26 @@
+package fi.dy.masa.malilib.util;
+
+import javax.annotation.Nullable;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
+
+public class EntityUtils
+{
+    /**
+     * Returns the camera entity, if it's not null, otherwise returns the client player entity.
+     * @return
+     */
+    @Nullable
+    public static Entity getCameraEntity()
+    {
+        Minecraft mc = Minecraft.getMinecraft();
+        Entity entity = mc.getRenderViewEntity();
+
+        if (entity == null)
+        {
+            entity = mc.player;
+        }
+
+        return entity;
+    }
+}
