987eb735c950572c49373e94ca6752614d341c4c
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2020-01-08 00:45:18 +0200

    Add FileRenamer and FileDeleter classes

--- src/main/java/fi/dy/masa/malilib/util/FileUtils.java
+++ src/main/java/fi/dy/masa/malilib/util/FileUtils.java
@@ -7,11 +7,14 @@ import java.util.Locale;
 import java.util.Set;
 import javax.annotation.Nullable;
 import com.google.common.collect.ImmutableSet;
-import com.mumfrey.liteloader.core.LiteLoader;
-import fi.dy.masa.malilib.LiteModMaLiLib;
 import net.minecraft.client.Minecraft;
 import net.minecraft.nbt.CompressedStreamTools;
 import net.minecraft.nbt.NBTTagCompound;
+import com.mumfrey.liteloader.core.LiteLoader;
+import fi.dy.masa.malilib.LiteModMaLiLib;
+import fi.dy.masa.malilib.gui.util.Message.MessageType;
+import fi.dy.masa.malilib.interfaces.IConfirmationListener;
+import fi.dy.masa.malilib.interfaces.IStringConsumerFeedback;
 
 public class FileUtils
 {
@@ -174,5 +177,90 @@ public class FileUtils
         }
 
         return null;
+    }
+
+    public static class FileRenamer implements IStringConsumerFeedback
+    {
+        protected final File dir;
+        protected final String oldName;
+
+        public FileRenamer(File dir, String oldName)
+        {
+            this.dir = dir;
+            this.oldName = oldName;
+        }
+
+        @Override
+        public boolean setString(String string)
+        {
+            if (FileUtils.doesFilenameContainIllegalCharacters(string))
+            {
+                InfoUtils.showGuiOrInGameMessage(MessageType.ERROR, "malilib.error.illegal_characters_in_file_name", string);
+                return false;
+            }
+
+            String newName = string;
+            int indexExt = this.oldName.lastIndexOf('.');
+            String ext = indexExt > 0 ? this.oldName.substring(indexExt) : null;
+
+            if (ext != null && newName.endsWith(ext) == false)
+            {
+                newName = newName + ext;
+            }
+
+            File newFile = new File(this.dir, newName);
+
+            if (newFile.exists() == false)
+            {
+                File oldFile = new File(this.dir, this.oldName);
+
+                if (oldFile.exists() && oldFile.canRead() && oldFile.renameTo(newFile))
+                {
+                    return true;
+                }
+                else
+                {
+                    InfoUtils.showGuiOrInGameMessage(MessageType.ERROR, "malilib.error.file_rename.rename_failed", this.oldName, newName);
+                }
+            }
+            else
+            {
+                InfoUtils.showGuiOrInGameMessage(MessageType.ERROR, "malilib.error.file_rename.file_already_exists", newName);
+            }
+
+            return false;
+        }
+    }
+
+    public static class FileDeleter implements IConfirmationListener
+    {
+        protected final File file;
+
+        public FileDeleter(File file)
+        {
+           this.file = file;
+        }
+
+        @Override
+        public boolean onActionCancelled()
+        {
+            return false;
+        }
+
+        @Override
+        public boolean onActionConfirmed()
+        {
+            try
+            {
+                this.file.delete();
+                return true;
+            }
+            catch (Exception e)
+            {
+                InfoUtils.showGuiOrInGameMessage(MessageType.ERROR, "malilib.error.file_delete_failed", this.file.getAbsolutePath());
+            }
+
+            return false;
+        }
     }
 }
