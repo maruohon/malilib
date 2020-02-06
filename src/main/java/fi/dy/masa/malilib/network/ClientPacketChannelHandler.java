97aba7f68618308a05ad319875f305bb9dc8c85b
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2020-02-06 21:05:18 +0200

    ClientPacketChannelHandler: Support multiple handlers per channel

--- src/main/java/fi/dy/masa/malilib/network/ClientPacketChannelHandler.java
+++ src/main/java/fi/dy/masa/malilib/network/ClientPacketChannelHandler.java
@@ -1,19 +1,19 @@
 package fi.dy.masa.malilib.network;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 import com.google.common.base.Charsets;
-import fi.dy.masa.malilib.MaLiLib;
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
+import com.google.common.collect.ArrayListMultimap;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.network.packet.CustomPayloadS2CPacket;
 import net.minecraft.server.network.packet.CustomPayloadC2SPacket;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.PacketByteBuf;
+import fi.dy.masa.malilib.MaLiLib;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 
 public class ClientPacketChannelHandler implements IClientPacketChannelHandler
 {
@@ -22,7 +22,7 @@ public class ClientPacketChannelHandler implements IClientPacketChannelHandler
 
     private static final ClientPacketChannelHandler INSTANCE = new ClientPacketChannelHandler();
 
-    private final HashMap<Identifier, IPluginChannelHandler> handlers;
+    private final ArrayListMultimap<Identifier, IPluginChannelHandler> handlers = ArrayListMultimap.create();
 
     public static IClientPacketChannelHandler getInstance()
     {
@@ -31,7 +31,6 @@ public class ClientPacketChannelHandler implements IClientPacketChannelHandler
 
     private ClientPacketChannelHandler()
     {
-        this.handlers = new HashMap<>();
     }
 
     @Override
@@ -41,7 +40,7 @@ public class ClientPacketChannelHandler implements IClientPacketChannelHandler
 
         for (Identifier channel : handler.getChannels())
         {
-            if (this.handlers.containsKey(channel) == false)
+            if (this.handlers.containsEntry(channel, handler) == false)
             {
                 this.handlers.put(channel, handler);
                 toRegister.add(channel);
@@ -79,16 +78,21 @@ public class ClientPacketChannelHandler implements IClientPacketChannelHandler
     public boolean processPacketFromServer(CustomPayloadS2CPacket packet, ClientPlayNetworkHandler netHandler)
     {
         Identifier channel = packet.getChannel();
-        IPluginChannelHandler handler = this.handlers.get(channel);
+        List<IPluginChannelHandler> handlers = this.handlers.get(channel);
 
-        if (handler != null)
+        if (handlers.isEmpty() == false)
         {
             PacketByteBuf buf = PacketSplitter.receive(netHandler, packet);
 
             // Finished the complete packet
             if (buf != null)
             {
-                handler.onPacketReceived(buf);
+                for (IPluginChannelHandler handler : handlers)
+                {
+                    buf.readerIndex(0);
+                    handler.onPacketReceived(buf);
+                    buf.readerIndex(0);
+                }
             }
 
             return true;
