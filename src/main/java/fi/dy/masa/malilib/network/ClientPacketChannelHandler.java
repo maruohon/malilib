9029ced49712d685156d8ebfb88bfa63cb342e0e
Arnevdl29 <arnevdl@gmail.com>
2020-02-10 21:15:54 +0100

    update yarn mappings to 1.15.2+build.12

--- src/main/java/fi/dy/masa/malilib/network/ClientPacketChannelHandler.java
+++ src/main/java/fi/dy/masa/malilib/network/ClientPacketChannelHandler.java
@@ -10,8 +10,8 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
-import net.minecraft.client.network.packet.CustomPayloadS2CPacket;
-import net.minecraft.server.network.packet.CustomPayloadC2SPacket;
+import net.minecraft.network.packet.s2c.play.CustomPayloadS2CPacket;
+import net.minecraft.network.packet.c2s.play.CustomPayloadC2SPacket;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.PacketByteBuf;
 
