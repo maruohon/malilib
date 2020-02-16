9fce9719a365072ddd9e9957ada91307c32aec39
Arnevdl29 <arnevdl@gmail.com>
2020-02-16 17:03:29 +0100

    fix format

--- src/main/java/fi/dy/masa/malilib/command/CommandBase.java
+++ src/main/java/fi/dy/masa/malilib/command/CommandBase.java
@@ -6,33 +6,38 @@ import net.minecraft.text.LiteralText;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
 
-public class CommandBase {
+public class CommandBase
+{
+    public static void localOutput(ServerCommandSource sender, String message)
+    {
+        sendColoredText(sender, Formatting.AQUA, message);
+    }
 
-	public static void localOutput(ServerCommandSource sender, String message) {
-		sendColoredText(sender, Formatting.AQUA, message);
-	}
+    public static void localOutputT(ServerCommandSource sender, String translationKey, Object... args)
+    {
+        sendColoredText(sender, Formatting.AQUA, new TranslatableText(translationKey, args));
+    }
 
-	public static void localOutputT(ServerCommandSource sender, String translationKey, Object... args) {
+    public static void localError(ServerCommandSource sender, String message)
+    {
+        sendColoredText(sender, Formatting.DARK_RED, message);
+    }
 
-		sendColoredText(sender, Formatting.AQUA, new TranslatableText(translationKey, args));
-	}
+    public static void localErrorT(ServerCommandSource sender, String translationKey, Object... args)
+    {
+        sendColoredText(sender, Formatting.DARK_RED, new TranslatableText(translationKey, args));
+    }
 
-	public static void localError(ServerCommandSource sender, String message) {
-		sendColoredText(sender, Formatting.DARK_RED, message);
-	}
+    public static void sendColoredText(ServerCommandSource sender, Formatting color, String message)
+    {
+        LiteralText chat = new LiteralText(message);
+        chat.formatted(color);
+        sender.getEntity().sendMessage(chat);
+    }
 
-	public static void localErrorT(ServerCommandSource sender, String translationKey, Object... args) {
-		sendColoredText(sender, Formatting.DARK_RED, new TranslatableText(translationKey, args));
-	}
-
-	public static void sendColoredText(ServerCommandSource sender, Formatting color, String message) {
-		LiteralText chat = new LiteralText(message);
-		chat.formatted(color);
-		sender.getEntity().sendMessage(chat);
-	}
-
-	public static void sendColoredText(ServerCommandSource sender, Formatting color, Text component) {
-		component.formatted(color);
-		sender.getEntity().sendMessage(component);
-	}
+    public static void sendColoredText(ServerCommandSource sender, Formatting color, Text component)
+    {
+        component.formatted(color);
+        sender.getEntity().sendMessage(component);
+    }
 }
--- src/main/java/fi/dy/masa/malilib/command/FakeCommandSource.java
+++ src/main/java/fi/dy/masa/malilib/command/FakeCommandSource.java
@@ -7,13 +7,16 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.server.command.ServerCommandSource;
 
-public class FakeCommandSource extends ServerCommandSource {
-	public FakeCommandSource(ClientPlayerEntity player) {
-		super(player, player.getPosVector(), player.getRotationClient(), null, 0, player.getEntityName(), player.getName(), null, player);
-	}
+public class FakeCommandSource extends ServerCommandSource
+{
+    public FakeCommandSource(ClientPlayerEntity player)
+    {
+        super(player, player.getPosVector(), player.getRotationClient(), null, 0, player.getEntityName(), player.getName(), null, player);
+    }
 
-	@Override
-	public Collection<String> getPlayerNames() {
-		return MinecraftClient.getInstance().getNetworkHandler().getPlayerList().stream().map(e -> e.getProfile().getName()).collect(Collectors.toList());
-	}
+    @Override
+    public Collection<String> getPlayerNames()
+    {
+        return MinecraftClient.getInstance().getNetworkHandler().getPlayerList().stream().map(e -> e.getProfile().getName()).collect(Collectors.toList());
+    }
 }
