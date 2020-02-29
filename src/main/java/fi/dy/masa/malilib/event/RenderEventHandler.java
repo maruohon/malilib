68f24e9cf5f6b3da0910a5f676885ce43899c136
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2020-02-29 22:27:09 +0200

    Add proper profiler sections for the custom rendering hooks
    The tooltip hook doesn't have one though, as it's not used that much...

--- src/main/java/fi/dy/masa/malilib/event/RenderEventHandler.java
+++ src/main/java/fi/dy/masa/malilib/event/RenderEventHandler.java
@@ -2,7 +2,6 @@ package fi.dy.masa.malilib.event;
 
 import java.util.ArrayList;
 import java.util.List;
-import net.minecraft.item.ItemStack;
 import fi.dy.masa.malilib.interfaces.IRenderDispatcher;
 import fi.dy.masa.malilib.interfaces.IRenderer;
 import fi.dy.masa.malilib.util.InfoUtils;
@@ -50,23 +49,31 @@ public class RenderEventHandler implements IRenderDispatcher
     /**
      * NOT PUBLIC API - DO NOT CALL
      */
-    public void onRenderGameOverlayPost(float partialTicks)
+    public void onRenderGameOverlayPost(net.minecraft.client.MinecraftClient mc, float partialTicks)
     {
+        mc.getProfiler().push("malilib_rendergameoverlaypost");
+
         if (this.overlayRenderers.isEmpty() == false)
         {
             for (IRenderer renderer : this.overlayRenderers)
             {
+                mc.getProfiler().push(renderer.getProfilerSectionSupplier());
                 renderer.onRenderGameOverlayPost(partialTicks);
+                mc.getProfiler().pop();
             }
         }
 
+        mc.getProfiler().push("malilib_ingamemessages");
         InfoUtils.renderInGameMessages();
+        mc.getProfiler().pop();
+
+        mc.getProfiler().pop();
     }
 
     /**
      * NOT PUBLIC API - DO NOT CALL
      */
-    public void onRenderTooltipLast(ItemStack stack, int x, int y)
+    public void onRenderTooltipLast(net.minecraft.item.ItemStack stack, int x, int y)
     {
         if (this.tooltipLastRenderers.isEmpty() == false)
         {
@@ -80,13 +87,17 @@ public class RenderEventHandler implements IRenderDispatcher
     /**
      * NOT PUBLIC API - DO NOT CALL
      */
-    public void onRenderWorldLast(net.minecraft.client.util.math.MatrixStack matrixStack, float partialTicks)
+    public void onRenderWorldLast(net.minecraft.client.util.math.MatrixStack matrixStack, net.minecraft.client.MinecraftClient mc, float partialTicks)
     {
         if (this.worldLastRenderers.isEmpty() == false)
         {
+            mc.getProfiler().swap("malilib_renderworldlast");
+
             for (IRenderer renderer : this.worldLastRenderers)
             {
+                mc.getProfiler().push(renderer.getProfilerSectionSupplier());
                 renderer.onRenderWorldLast(partialTicks, matrixStack);
+                mc.getProfiler().pop();
             }
         }
     }
--- src/main/java/fi/dy/masa/malilib/mixin/MixinInGameHud.java
+++ src/main/java/fi/dy/masa/malilib/mixin/MixinInGameHud.java
@@ -1,18 +1,21 @@
 package fi.dy.masa.malilib.mixin;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import fi.dy.masa.malilib.event.RenderEventHandler;
-import net.minecraft.client.gui.hud.InGameHud;
 
-@Mixin(InGameHud.class)
+@Mixin(net.minecraft.client.gui.hud.InGameHud.class)
 public abstract class MixinInGameHud
 {
+    @Shadow @Final private net.minecraft.client.MinecraftClient client;
+
     @Inject(method = "render", at = @At("RETURN"))
     private void onGameOverlayPost(float partialTicks, CallbackInfo ci)
     {
-        ((RenderEventHandler) RenderEventHandler.getInstance()).onRenderGameOverlayPost(partialTicks);
+        ((RenderEventHandler) RenderEventHandler.getInstance()).onRenderGameOverlayPost(this.client, partialTicks);
     }
 }
