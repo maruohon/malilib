22a29cf98b8dd103c464a9c87a141e6fb459ce37
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2020-06-12 23:43:11 +0300

    Remove commented out testing code

--- src/main/java/fi/dy/masa/malilib/render/RenderObjectVbo.java
+++ src/main/java/fi/dy/masa/malilib/render/RenderObjectVbo.java
@@ -33,39 +33,12 @@ public class RenderObjectVbo extends RenderObjectBase
     @Override
     public void draw()
     {
-        /*
-        for (VertexFormatElement element : this.getVertexFormat().getElements())
-        {
-            VertexFormatElement.EnumUsage usage = element.getUsage();
-
-            switch (usage)
-            {
-                case POSITION:
-                    GlStateManager.glEnableClientState(GL11.GL_VERTEX_ARRAY);
-                    break;
-                case UV:
-                    OpenGlHelper.setClientActiveTexture(OpenGlHelper.defaultTexUnit);
-                    GlStateManager.glEnableClientState(GL11.GL_TEXTURE_COORD_ARRAY);
-                    OpenGlHelper.setClientActiveTexture(OpenGlHelper.lightmapTexUnit);
-                    GlStateManager.glEnableClientState(GL11.GL_TEXTURE_COORD_ARRAY);
-                    OpenGlHelper.setClientActiveTexture(OpenGlHelper.defaultTexUnit);
-                    break;
-                case COLOR:
-                    GlStateManager.glEnableClientState(GL11.GL_COLOR_ARRAY);
-                default:
-            }
-        }
-        */
-
         if (this.hasTexture)
         {
             GlStateManager.enableTexture2D();
 
             OpenGlHelper.setClientActiveTexture(OpenGlHelper.defaultTexUnit);
             GlStateManager.glEnableClientState(GL11.GL_TEXTURE_COORD_ARRAY);
-            //OpenGlHelper.setClientActiveTexture(OpenGlHelper.lightmapTexUnit);
-            //GlStateManager.glEnableClientState(GL11.GL_TEXTURE_COORD_ARRAY);
-            //OpenGlHelper.setClientActiveTexture(OpenGlHelper.defaultTexUnit);
         }
         else
         {
@@ -80,35 +53,7 @@ public class RenderObjectVbo extends RenderObjectBase
         {
             OpenGlHelper.setClientActiveTexture(OpenGlHelper.defaultTexUnit);
             GlStateManager.glDisableClientState(GL11.GL_TEXTURE_COORD_ARRAY);
-            //OpenGlHelper.setClientActiveTexture(OpenGlHelper.defaultTexUnit);
         }
-
-        /*
-        OpenGlHelper.glBindBuffer(OpenGlHelper.GL_ARRAY_BUFFER, 0);
-
-        for (VertexFormatElement element : this.getVertexFormat().getElements())
-        {
-            VertexFormatElement.EnumUsage usage = element.getUsage();
-
-            switch (usage)
-            {
-                case POSITION:
-                    GlStateManager.glDisableClientState(GL11.GL_VERTEX_ARRAY);
-                    break;
-                case UV:
-                    OpenGlHelper.setClientActiveTexture(OpenGlHelper.defaultTexUnit + element.getIndex());
-                    GlStateManager.glDisableClientState(GL11.GL_TEXTURE_COORD_ARRAY);
-                    OpenGlHelper.setClientActiveTexture(OpenGlHelper.lightmapTexUnit);
-                    GlStateManager.glDisableClientState(GL11.GL_TEXTURE_COORD_ARRAY);
-                    OpenGlHelper.setClientActiveTexture(OpenGlHelper.defaultTexUnit);
-                    break;
-                case COLOR:
-                    GlStateManager.glDisableClientState(GL11.GL_COLOR_ARRAY);
-                    GlStateManager.resetColor();
-                default:
-            }
-        }
-        */
     }
 
     @Override
