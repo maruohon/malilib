b117741e01c8b3154f278e5b2c0c4440b83936fe
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2020-06-24 21:36:14 +0300

    Change the render world last injection to fix rendering issues on Fabulous

--- src/main/java/fi/dy/masa/malilib/mixin/MixinWorldRenderer.java
+++ src/main/java/fi/dy/masa/malilib/mixin/MixinWorldRenderer.java
@@ -5,6 +5,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import fi.dy.masa.malilib.event.RenderEventHandler;
 
@@ -14,9 +15,28 @@ public abstract class MixinWorldRenderer
     @Shadow @Final private net.minecraft.client.MinecraftClient client;
 
     @Inject(method = "render",
-            at = @At(value = "INVOKE_STRING", args = "ldc=weather",
-                     target = "Lnet/minecraft/util/profiler/Profiler;swap(Ljava/lang/String;)V"))
-    private void onRenderWorldLast(
+            at = @At(value = "INVOKE", ordinal = 1,
+                     target = "Lnet/minecraft/client/render/WorldRenderer;renderWeather(Lnet/minecraft/client/render/LightmapTextureManager;FDDD)V"))
+    private void onRenderWorldLastNormal(
+            net.minecraft.client.util.math.MatrixStack matrices,
+            float tickDelta, long limitTime, boolean renderBlockOutline,
+            net.minecraft.client.render.Camera camera,
+            net.minecraft.client.render.GameRenderer gameRenderer,
+            net.minecraft.client.render.LightmapTextureManager lightmapTextureManager,
+            net.minecraft.util.math.Matrix4f matrix4f,
+            CallbackInfo ci)
+    {
+        ((RenderEventHandler) RenderEventHandler.getInstance()).onRenderWorldLast(matrices, this.client, tickDelta);
+    }
+
+    @Inject(method = "render",
+            slice = @Slice(from = @At(value = "FIELD", ordinal = 1, // start from the endDrawing() call
+                                      target = "Lnet/minecraft/client/render/RenderPhase;WEATHER_TARGET:Lnet/minecraft/client/render/RenderPhase$Target;"),
+                            to = @At(value = "INVOKE", ordinal = 1, // end at the second renderWeather call
+                                     target = "Lnet/minecraft/client/render/WorldRenderer;renderWeather(Lnet/minecraft/client/render/LightmapTextureManager;FDDD)V")),
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/client/gl/ShaderEffect;render(F)V"))
+    private void onRenderWorldLastFabulous(
             net.minecraft.client.util.math.MatrixStack matrices,
             float tickDelta, long limitTime, boolean renderBlockOutline,
             net.minecraft.client.render.Camera camera,
