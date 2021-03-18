ad84be46167fcc41bb8a7b10099b99c2b316eb66
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2021-03-18 22:36:22 +0200

    Make the ColorIndicatorWidget a bit more generic use/cleaner

--- src/main/java/fi/dy/masa/malilib/gui/widget/ColorIndicatorWidget.java
+++ src/main/java/fi/dy/masa/malilib/gui/widget/ColorIndicatorWidget.java
@@ -1,6 +1,8 @@
 package fi.dy.masa.malilib.gui.widget;
 
 import java.util.function.IntConsumer;
+import java.util.function.IntSupplier;
+import javax.annotation.Nullable;
 import fi.dy.masa.malilib.config.option.IntegerConfig;
 import fi.dy.masa.malilib.gui.BaseScreen;
 import fi.dy.masa.malilib.gui.ColorEditorHSVScreen;
@@ -10,21 +12,34 @@ import fi.dy.masa.malilib.util.data.Color4f;
 
 public class ColorIndicatorWidget extends InteractableWidget
 {
-    protected final IntegerConfig config;
+    protected final IntSupplier valueSupplier;
     protected final IntConsumer valueConsumer;
+    @Nullable protected IntegerConfig config;
 
     public ColorIndicatorWidget(int x, int y, int width, int height, Color4f color, IntConsumer consumer)
     {
-        this(x, y, width, height, new IntegerConfig("", color.intValue), consumer);
+        this(x, y, width, height, color.intValue, consumer);
+    }
+
+    public ColorIndicatorWidget(int x, int y, int width, int height, int color, IntConsumer consumer)
+    {
+        this(x, y, width, height, () -> color, consumer);
     }
 
     public ColorIndicatorWidget(int x, int y, int width, int height, IntegerConfig config, IntConsumer consumer)
     {
-        super(x, y, width, height);
+        this(x, y, width, height, config::getIntegerValue, consumer);
 
         this.config = config;
+    }
+
+    public ColorIndicatorWidget(int x, int y, int width, int height, IntSupplier valueSupplier, IntConsumer consumer)
+    {
+        super(x, y, width, height);
+
+        this.valueSupplier = valueSupplier;
         this.valueConsumer = consumer;
-        String color = Color4f.getHexColorString(this.config.getIntegerValue());
+        String color = Color4f.getHexColorString(valueSupplier.getAsInt());
         this.translateAndAddHoverString("malilib.gui.hover.open_color_editor", color);
 
         this.setClickListener(this::openColorEditorScreen);
@@ -32,9 +47,9 @@ public class ColorIndicatorWidget extends InteractableWidget
 
     protected void openColorEditorScreen()
     {
-        if (this.config.isLocked() == false)
+        if (this.config == null || this.config.isLocked() == false)
         {
-            ColorEditorHSVScreen gui = new ColorEditorHSVScreen(this.config.getIntegerValue(), this.valueConsumer, null, GuiUtils.getCurrentScreen());
+            ColorEditorHSVScreen gui = new ColorEditorHSVScreen(this.valueSupplier.getAsInt(), this.valueConsumer, null, GuiUtils.getCurrentScreen());
             BaseScreen.openPopupScreen(gui);
         }
     }
@@ -47,6 +62,6 @@ public class ColorIndicatorWidget extends InteractableWidget
 
         ShapeRenderUtils.renderRectangle(x    , y    , z, width    , height    , 0xFFFFFFFF);
         ShapeRenderUtils.renderRectangle(x + 1, y + 1, z, width - 2, height - 2, 0xFF000000);
-        ShapeRenderUtils.renderRectangle(x + 2, y + 2, z, width - 4, height - 4, 0xFF000000 | this.config.getIntegerValue());
+        ShapeRenderUtils.renderRectangle(x + 2, y + 2, z, width - 4, height - 4, 0xFF000000 | this.valueSupplier.getAsInt());
     }
 }
