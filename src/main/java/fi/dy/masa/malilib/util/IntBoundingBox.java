3f06d889dee001c8054bd09d3e5325140813fc76
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2021-04-04 14:02:20 +0300

    Update to MC 1.17 snapshot 21w13a

--- src/main/java/fi/dy/masa/malilib/util/IntBoundingBox.java
+++ src/main/java/fi/dy/masa/malilib/util/IntBoundingBox.java
@@ -55,7 +55,7 @@ public class IntBoundingBox
 
     public static IntBoundingBox fromVanillaBox(BlockBox box)
     {
-        return createProper(box.minX, box.minY, box.minZ, box.maxX, box.maxY, box.maxZ);
+        return createProper(box.getMinX(), box.getMinY(), box.getMinZ(), box.getMaxX(), box.getMaxY(), box.getMaxZ());
     }
 
     public static IntBoundingBox createProper(int x1, int y1, int z1, int x2, int y2, int z2)
