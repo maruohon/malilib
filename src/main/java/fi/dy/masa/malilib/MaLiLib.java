94545bf064b191a20f125248b319a1cc9ac4b1ed
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2021-07-09 15:52:32 +0300

    Add a "Hide All Coordinates" config option for other mods to use

--- src/main/java/fi/dy/masa/malilib/MaLiLib.java
+++ src/main/java/fi/dy/masa/malilib/MaLiLib.java
@@ -6,4 +6,9 @@ import org.apache.logging.log4j.Logger;
 public class MaLiLib
 {
     public static final Logger LOGGER = LogManager.getLogger(MaLiLibReference.MOD_ID);
+
+    public static boolean canShowCoordinates()
+    {
+        return MaLiLibConfigs.Generic.HIDE_ALL_COORDINATES.getBooleanValue() == false;
+    }
 }
--- src/main/java/fi/dy/masa/malilib/MaLiLibConfigs.java
+++ src/main/java/fi/dy/masa/malilib/MaLiLibConfigs.java
@@ -38,6 +38,7 @@ public class MaLiLibConfigs
         public static final IntegerConfig CUSTOM_SCREEN_SCALE                   = new IntegerConfig("customScreenScale", 2, 0, 8);
         public static final StringConfig DATA_DUMP_CSV_DELIMITER                = new StringConfig("dataDumpCsvDelimiter", ",");
         public static final BooleanConfig DROP_DOWN_SEARCH_TIP                  = new BooleanConfig("dropDownSearchTip", true);
+        public static final BooleanConfig HIDE_ALL_COORDINATES                  = new BooleanConfig("hideAllCoordinates", false);
         public static final IntegerConfig HOVER_TEXT_MAX_WIDTH                  = new IntegerConfig("hoverTextMaxWidth", 310, 16, 4096);
         public static final BooleanConfig KEYBIND_DISPLAY_CALLBACK_ONLY         = new BooleanConfig("keybindDisplayCallbackOnly", true);
         public static final BooleanConfig KEYBIND_DISPLAY_CANCEL_ONLY           = new BooleanConfig("keybindDisplayCancelOnly", true);
@@ -60,6 +61,7 @@ public class MaLiLibConfigs
                 CUSTOM_SCREEN_SCALE,
                 DATA_DUMP_CSV_DELIMITER,
                 DROP_DOWN_SEARCH_TIP,
+                HIDE_ALL_COORDINATES,
                 HOVER_TEXT_MAX_WIDTH,
                 KEYBIND_DISPLAY,
                 KEYBIND_DISPLAY_CALLBACK_ONLY,
