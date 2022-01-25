5f4c9b4b0bfc28ee744d82e77867fc87efce8e22
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2022-01-25 10:18:51 +0200

    Fix the onRenderTooltipLast hook not getting called in the Creative search tab

--- src/main/java/fi/dy/masa/malilib/mixin/MixinCreativeInventoryScreen.java
+++ src/main/java/fi/dy/masa/malilib/mixin/MixinCreativeInventoryScreen.java
@@ -1,20 +1,20 @@
 package fi.dy.masa.malilib.mixin;
 
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import net.minecraft.client.gui.AbstractParentElement;
-import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
 import fi.dy.masa.malilib.event.RenderEventHandler;
 
-@Mixin(Screen.class)
-public abstract class MixinScreen extends AbstractParentElement
+@Mixin(CreativeInventoryScreen.class)
+public abstract class MixinCreativeInventoryScreen
 {
-    @Inject(method = "renderTooltip(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/item/ItemStack;II)V",
-            at = @At("RETURN"))
+    @Inject(method = "renderTooltip", at = @At(value = "INVOKE", shift = At.Shift.AFTER,
+            target = "Lnet/minecraft/client/gui/screen/ingame/CreativeInventoryScreen;renderTooltip(Lnet/minecraft/client/util/math/MatrixStack;Ljava/util/List;Ljava/util/Optional;II)V"))
     private void onRenderTooltip(MatrixStack matrixStack, ItemStack stack, int x, int y, CallbackInfo ci)
     {
         ((RenderEventHandler) RenderEventHandler.getInstance()).onRenderTooltipLast(matrixStack, stack, x, y);
