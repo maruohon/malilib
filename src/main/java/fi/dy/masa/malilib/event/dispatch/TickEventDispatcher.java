7979e821a07343237f197197ac3d64f156af6156
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2022-08-19 23:46:52 +0300

    Update a few Javadocs

--- src/main/java/fi/dy/masa/malilib/event/dispatch/TickEventDispatcher.java
+++ src/main/java/fi/dy/masa/malilib/event/dispatch/TickEventDispatcher.java
@@ -5,7 +5,7 @@ import fi.dy.masa.malilib.event.ClientTickHandler;
 public interface TickEventDispatcher
 {
     /**
-     * Registers a client tick handler, which will have its {@link ClientTickHandler#onClientTick(net.minecraft.client.Minecraft)} method
+     * Registers a client tick handler, which will have its {@link ClientTickHandler#onClientTick()} method
      * called at the end of the client world ticking phase.
      * @param handler
      */
