cf38d1d47412038abbba20c5fbb21eda0772704b
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2023-03-07 19:26:49 +0200

    malilib configs class white-space clean-up

--- src/main/java/malilib/MaLiLibConfigs.java
+++ src/main/java/malilib/MaLiLibConfigs.java
@@ -26,37 +26,37 @@ public class MaLiLibConfigs
 
     public static class Generic
     {
-        public static final OptionListConfig<Scope> CONFIG_SEARCH_DEFAULT_SCOPE             = new OptionListConfig<>("configSearchDefaultScope", Scope.ALL_CATEGORIES, Scope.VALUES);
-        public static final OptionListConfig<MessageOutput> DEFAULT_TOGGLE_MESSAGE_OUTPUT   = new OptionListConfig<>("defaultToggleMessageOutput", MessageOutput.CUSTOM_HOTBAR, getUsableDefaultToggleMessageOutputs());
-        public static final OptionListConfig<KeybindDisplayMode> KEYBIND_DISPLAY            = new OptionListConfig<>("keybindDisplay", KeybindDisplayMode.NONE, KeybindDisplayMode.VALUES);
+        public static final OptionListConfig<Scope>              CONFIG_SEARCH_DEFAULT_SCOPE    = new OptionListConfig<>("configSearchDefaultScope", Scope.ALL_CATEGORIES, Scope.VALUES);
+        public static final OptionListConfig<MessageOutput>      DEFAULT_TOGGLE_MESSAGE_OUTPUT  = new OptionListConfig<>("defaultToggleMessageOutput", MessageOutput.CUSTOM_HOTBAR, getUsableDefaultToggleMessageOutputs());
+        public static final OptionListConfig<KeybindDisplayMode> KEYBIND_DISPLAY                = new OptionListConfig<>("keybindDisplay", KeybindDisplayMode.NONE, KeybindDisplayMode.VALUES);
 
-        public static final BooleanConfig ACTION_PROMPT_FUZZY_SEARCH            = new BooleanConfig("actionPromptFuzzySearch", false);
-        public static final BooleanConfig ACTION_PROMPT_REMEMBER_SEARCH         = new BooleanConfig("actionPromptRememberSearch", false);
-        public static final BooleanConfig ACTION_PROMPT_SEARCH_DISPLAY_NAME     = new BooleanConfig("actionPromptSearchDisplayName", false);
-        public static final BooleanConfig CONFIG_BACKUP_ANTI_DUPLICATE          = new BooleanConfig("configBackupAntiDuplicate", true);
-        public static final IntegerConfig CONFIG_BACKUP_COUNT                   = new IntegerConfig("configBackupCount", 20, 0, 200);
-        public static final BooleanConfig CONFIG_WIDGET_BACKGROUND              = new BooleanConfig("configWidgetBackground", true);
-        public static final IntegerConfig CUSTOM_HOTBAR_MESSAGE_LIMIT           = new IntegerConfig("customHotbarMessageLimit", 3, 1, 16);
-        public static final IntegerConfig CUSTOM_SCREEN_SCALE                   = new IntegerConfig("customScreenScale", 2, 0, 8);
-        public static final StringConfig DATA_DUMP_CSV_DELIMITER               = new StringConfig("dataDumpCsvDelimiter", ",");
-        public static final BooleanConfig DROP_DOWN_SEARCH_TIP                  = new BooleanConfig("dropDownSearchTip", true);
-        public static final BooleanConfig FILE_BROWSER_SHOW_HIDDEN_FILES        = new BooleanConfig("fileBrowserShowHiddenFiles", false);
-        public static final BooleanConfig HIDE_ALL_COORDINATES                  = new BooleanConfig("hideAllCoordinates", false);
-        public static final ColorConfig HOVERED_LIST_ENTRY_COLOR              = new ColorConfig("hoveredListEntryColor", "#C0404040");
-        public static final IntegerConfig HOVER_TEXT_MAX_WIDTH                  = new IntegerConfig("hoverTextMaxWidth", 310, 16, 4096);
-        public static final BooleanConfig KEYBIND_DISPLAY_CALLBACK_ONLY         = new BooleanConfig("keybindDisplayCallbackOnly", true);
-        public static final BooleanConfig KEYBIND_DISPLAY_CANCEL_ONLY           = new BooleanConfig("keybindDisplayCancelOnly", true);
-        public static final IntegerConfig KEYBIND_DISPLAY_DURATION              = new IntegerConfig("keybindDisplayDuration", 5000, 0, 120000);
-        public static final IntegerConfig MESSAGE_FADE_OUT_TIME                 = new IntegerConfig("messageFadeOutTime", 500, 0, 10000);
-        public static final BooleanConfig OPTION_LIST_CONFIG_USE_DROPDOWN       = new BooleanConfig("optionListConfigUseDropdown", false);
-        public static final BooleanConfig REALMS_COMMON_CONFIG                  = new BooleanConfig("realmsCommonConfig", true);
-        public static final BooleanConfig REMEMBER_CONFIG_TAB_SCROLL_POSITIONS  = new BooleanConfig("rememberConfigTabScrollPositions", true);
-        public static final BooleanConfig REMEMBER_FILE_BROWSER_SCROLL_POSITIONS= new BooleanConfig("rememberFileBrowserScrollPositions", true);
-        public static final ColorConfig   SELECTED_LIST_ENTRY_COLOR             = new ColorConfig(  "selectedListEntryColor", "#FFFFFFFF");
-        public static final BooleanConfig SERVER_MESSAGES                       = new BooleanConfig("serverMessages", true);
-        public static final BooleanConfig SHOW_INTERNAL_CONFIG_NAME             = new BooleanConfig("showInternalConfigName", false);
-        public static final BooleanConfig SORT_CONFIGS_BY_NAME                  = new BooleanConfig("sortConfigsByName", false);
-        public static final BooleanConfig SORT_EXTENSION_MOD_OPTIONS            = new BooleanConfig("sortExtensionModOptions", false);
+        public static final BooleanConfig ACTION_PROMPT_FUZZY_SEARCH                = new BooleanConfig("actionPromptFuzzySearch", false);
+        public static final BooleanConfig ACTION_PROMPT_REMEMBER_SEARCH             = new BooleanConfig("actionPromptRememberSearch", false);
+        public static final BooleanConfig ACTION_PROMPT_SEARCH_DISPLAY_NAME         = new BooleanConfig("actionPromptSearchDisplayName", false);
+        public static final BooleanConfig CONFIG_BACKUP_ANTI_DUPLICATE              = new BooleanConfig("configBackupAntiDuplicate", true);
+        public static final IntegerConfig CONFIG_BACKUP_COUNT                       = new IntegerConfig("configBackupCount", 20, 0, 200);
+        public static final BooleanConfig CONFIG_WIDGET_BACKGROUND                  = new BooleanConfig("configWidgetBackground", true);
+        public static final IntegerConfig CUSTOM_HOTBAR_MESSAGE_LIMIT               = new IntegerConfig("customHotbarMessageLimit", 3, 1, 16);
+        public static final IntegerConfig CUSTOM_SCREEN_SCALE                       = new IntegerConfig("customScreenScale", 2, 0, 8);
+        public static final StringConfig  DATA_DUMP_CSV_DELIMITER                   = new StringConfig("dataDumpCsvDelimiter", ",");
+        public static final BooleanConfig DROP_DOWN_SEARCH_TIP                      = new BooleanConfig("dropDownSearchTip", true);
+        public static final BooleanConfig FILE_BROWSER_SHOW_HIDDEN_FILES            = new BooleanConfig("fileBrowserShowHiddenFiles", false);
+        public static final BooleanConfig HIDE_ALL_COORDINATES                      = new BooleanConfig("hideAllCoordinates", false);
+        public static final ColorConfig   HOVERED_LIST_ENTRY_COLOR                  = new ColorConfig("hoveredListEntryColor", "#C0404040");
+        public static final IntegerConfig HOVER_TEXT_MAX_WIDTH                      = new IntegerConfig("hoverTextMaxWidth", 310, 16, 4096);
+        public static final BooleanConfig KEYBIND_DISPLAY_CALLBACK_ONLY             = new BooleanConfig("keybindDisplayCallbackOnly", true);
+        public static final BooleanConfig KEYBIND_DISPLAY_CANCEL_ONLY               = new BooleanConfig("keybindDisplayCancelOnly", true);
+        public static final IntegerConfig KEYBIND_DISPLAY_DURATION                  = new IntegerConfig("keybindDisplayDuration", 5000, 0, 120000);
+        public static final IntegerConfig MESSAGE_FADE_OUT_TIME                     = new IntegerConfig("messageFadeOutTime", 500, 0, 10000);
+        public static final BooleanConfig OPTION_LIST_CONFIG_USE_DROPDOWN           = new BooleanConfig("optionListConfigUseDropdown", false);
+        public static final BooleanConfig REALMS_COMMON_CONFIG                      = new BooleanConfig("realmsCommonConfig", true);
+        public static final BooleanConfig REMEMBER_CONFIG_TAB_SCROLL_POSITIONS      = new BooleanConfig("rememberConfigTabScrollPositions", true);
+        public static final BooleanConfig REMEMBER_FILE_BROWSER_SCROLL_POSITIONS    = new BooleanConfig("rememberFileBrowserScrollPositions", true);
+        public static final ColorConfig   SELECTED_LIST_ENTRY_COLOR                 = new ColorConfig(  "selectedListEntryColor", "#FFFFFFFF");
+        public static final BooleanConfig SERVER_MESSAGES                           = new BooleanConfig("serverMessages", true);
+        public static final BooleanConfig SHOW_INTERNAL_CONFIG_NAME                 = new BooleanConfig("showInternalConfigName", false);
+        public static final BooleanConfig SORT_CONFIGS_BY_NAME                      = new BooleanConfig("sortConfigsByName", false);
+        public static final BooleanConfig SORT_EXTENSION_MOD_OPTIONS                = new BooleanConfig("sortExtensionModOptions", false);
 
         public static final ImmutableList<ConfigOption<?>> OPTIONS = ImmutableList.of(
                 ACTION_PROMPT_FUZZY_SEARCH,
@@ -96,12 +96,12 @@ public class MaLiLibConfigs
     {
         public static final KeyBindSettings SCROLL_ADJUST = KeyBindSettings.builder().extra().cancel(CancelCondition.ON_SUCCESS).noOutput().build();
 
-        public static final HotkeyConfig IGNORED_KEYS                           = new HotkeyConfig("ignoredKeys", "");
-        public static final HotkeyConfig OPEN_ACTION_PROMPT_SCREEN              = new HotkeyConfig("openActionPromptScreen", "");
-        public static final HotkeyConfig OPEN_CONFIG_SCREEN                     = new HotkeyConfig("openConfigScreen", "A,C");
-        public static final HotkeyConfig SCROLL_VALUE_ADJUST_DECREASE           = new HotkeyConfig("scrollValueAdjustDecrease", "SCROLL_DOWN", SCROLL_ADJUST);
-        public static final HotkeyConfig SCROLL_VALUE_ADJUST_INCREASE           = new HotkeyConfig("scrollValueAdjustIncrease", "SCROLL_UP", SCROLL_ADJUST);
-        public static final HotkeyConfig SCROLL_VALUE_ADJUST_MODIFIER           = new HotkeyConfig("scrollValueAdjustModifier", "", KeyBindSettings.INGAME_MODIFIER_EMPTY);
+        public static final HotkeyConfig IGNORED_KEYS                   = new HotkeyConfig("ignoredKeys", "");
+        public static final HotkeyConfig OPEN_ACTION_PROMPT_SCREEN      = new HotkeyConfig("openActionPromptScreen", "");
+        public static final HotkeyConfig OPEN_CONFIG_SCREEN             = new HotkeyConfig("openConfigScreen", "A,C");
+        public static final HotkeyConfig SCROLL_VALUE_ADJUST_DECREASE   = new HotkeyConfig("scrollValueAdjustDecrease", "SCROLL_DOWN", SCROLL_ADJUST);
+        public static final HotkeyConfig SCROLL_VALUE_ADJUST_INCREASE   = new HotkeyConfig("scrollValueAdjustIncrease", "SCROLL_UP", SCROLL_ADJUST);
+        public static final HotkeyConfig SCROLL_VALUE_ADJUST_MODIFIER   = new HotkeyConfig("scrollValueAdjustModifier", "", KeyBindSettings.INGAME_MODIFIER_EMPTY);
 
         public static final ImmutableList<HotkeyConfig> FUNCTIONAL_HOTKEYS = ImmutableList.of(
                 OPEN_ACTION_PROMPT_SCREEN,
