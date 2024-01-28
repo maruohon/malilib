5dc9b9fc9547722468a8235d42bc4dfcc85c159c
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2024-01-28 21:22:57 +0200

    Back-porting to Beta 1.7.3, part 12: Init the TextRenderer on game load

--- src/main/java/malilib/MaLiLibInitHandler.java
+++ src/main/java/malilib/MaLiLibInitHandler.java
@@ -5,11 +5,13 @@ import malilib.input.CustomHotkeyManager;
 import malilib.network.message.ConfigLockPacketHandler;
 import malilib.overlay.widget.ConfigStatusIndicatorContainerWidget;
 import malilib.registry.Registry;
+import malilib.render.text.TextRenderer;
 
 public class MaLiLibInitHandler
 {
     public static void registerMalilibHandlers()
     {
+        TextRenderer.INSTANCE.onResourceManagerReload(); // TODO b1.7.3 port FIXME move this to some sane place...
         Registry.CONFIG_MANAGER.registerConfigHandler(BaseModConfig.createDefaultModConfig(MaLiLibReference.MOD_INFO, MaLiLibConfigs.CONFIG_VERSION, MaLiLibConfigs.CATEGORIES));
         Registry.CONFIG_SCREEN.registerConfigScreenFactory(MaLiLibReference.MOD_INFO, MaLiLibConfigScreen::create);
         Registry.CONFIG_TAB.registerConfigTabSupplier(MaLiLibReference.MOD_INFO, MaLiLibConfigScreen::getConfigTabs);
