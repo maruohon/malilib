6d12e2549345f12cecd032d30d3aa8de14189345
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2024-01-28 16:39:11 +0200

    Back-porting to Beta 1.7.3, part 7: Prevent crashes with missing textures
    By default if a non-existing texture is loaded, and the TextureManager
    is reloaded, the game will crash.

---
+++ src/main/java/malilib/mixin/fix/TextureManagerMixin.java
@@ -0,0 +1,43 @@
+package malilib.mixin.fix;
+
+import java.awt.image.BufferedImage;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import javax.imageio.ImageIO;
+import org.apache.commons.lang3.tuple.Pair;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.client.render.texture.TextureManager;
+
+@Mixin(TextureManager.class)
+public class TextureManagerMixin
+{
+    @Shadow private BufferedImage image;
+
+    @Inject(method = "readImage", at = @At("HEAD"), cancellable = true)
+    private void malilib_preventCrashes(InputStream is, CallbackInfoReturnable<BufferedImage> cir)
+    {
+        System.out.printf("TextureManager#readImage(%s)\n", is);
+        if (is == null)
+        {
+            cir.setReturnValue(this.image);
+        }
+
+        try
+        {
+            BufferedImage img = ImageIO.read(is);
+            is.close();
+            cir.setReturnValue(img);
+        }
+        catch (Exception e)
+        {
+            cir.setReturnValue(this.image);
+        }
+    }
