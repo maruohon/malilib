eb6769f511faf872901d4dfcd294fc316e1d0250
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2024-02-05 00:13:54 +0200

    Move `getStackString()` from `ItemUtils` to `ItemWrap`

--- src/main/java/malilib/util/game/wrap/ItemWrap.java
+++ src/main/java/malilib/util/game/wrap/ItemWrap.java
@@ -32,4 +32,19 @@ public class ItemWrap
     {
         return stack.isEmpty() == false;
     }
+
+    public static String getStackString(ItemStack stack)
+    {
+        if (ItemWrap.notEmpty(stack))
+        {
+            String id = RegistryUtils.getItemIdStr(stack.getItem());
+            NBTTagCompound tag = ItemWrap.getTag(stack);
+
+            return String.format("[%s @ %d - display: %s - NBT: %s] (%s)",
+                                 id != null ? id : "null", stack.getMetadata(), stack.getDisplayName(),
+                                 tag != null ? tag.toString() : "<no NBT>", stack);
+        }
+
+        return "<empty>";
+    }
 }
